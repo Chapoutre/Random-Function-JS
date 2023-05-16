e341e528484e52976a848891371b7d71732bcede
Zffu <103074097+Zffu@users.noreply.github.com>
2023-05-16 17:18:06 +0200

    Upgraded Code to be a bit faster

--- random.js
+++ random.js
@@ -1,5 +1,3 @@
 function getRandInt (min, max) {
-    let r = Math.round(Math.random())
-    const calcul = r*(max - min) + min
-    return calcul
+    return Math.round(Math.random() * max) + min
 }
